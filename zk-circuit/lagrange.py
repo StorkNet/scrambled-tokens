d91d0d2392f1dce8d10b6eec0c2aef122d92d8dd
Shankar <shankars0899@gmail.com>
2022-10-29 14:24:14 -0400

    added rand as challenge

--- zk-circuit/lagrange.py
+++ zk-circuit/lagrange.py
@@ -2,16 +2,29 @@ import numpy as np
 
 class LagrangePoly:
 
-    def __init__(self, X, Y):
+    def __init__(self, X):
         self.n = len(X)
         self.X = np.array(X)
-        self.D_Y = np.array(Y)
+
+        self.D_Y = []
+
+        for i in range(self.n):
+            challenge = 1
+            while self.X[i] % challenge == 0:
+                challenge = self.randNum()
+            self.D_Y.append(challenge)
+
         self.F_Y = np.ones(len(X), dtype=int)
+
         assert(len(X) == len(self.D_Y) == len(self.F_Y))
+
+    def randNum(self):
+        return np.random.randint(low=0xFF, high=0xFFFF, size=1, dtype=int)
 
     def Dx(self, x, j):
         b = [(x - self.X[m]) / (self.X[j] - self.X[m])
              for m in range(self.n) if m != j]
+
         return np.prod(b, axis=0) * self.D_Y[j]
 
     def Fx(self, x, j):
@@ -23,11 +36,21 @@ class LagrangePoly:
         Dy = [self.Dx(x, j) for j in range(self.n)]
         Fy = [self.Fx(x, j) for j in range(self.n)]
 
-        return np.sum(Dy, axis=0)
+        Fy = np.sum(Fy, axis=0)
+
+        if Fy != 1:
+            Fy = 0
+
+        return (np.sum(Dy, axis=0) * Fy)[0]
 
 '''
-X = [0x6be986730E72fCd23910D66A1722b4b3611e50D2, 0x6be986730E72fCd23910D66A1722b4b3611e50D2, 0x5F45498761247C2A4609d66E633959e0f9999999, 0x1ab07e29D2ec76117704944DfBDB9dE5b785E8f4, 0xD3358467C97906bA732d0ca4af20f14F0424CD4F]
-Dx_Range = [0.3, 11, 0.57, 120, 17]
+X = [0x6be986730E72fCd23910D66A1722b4b3611e50D2, 0x07F20B1265059fEb39D605e8476aa51595483Ec6, 0x5F45498761247C2A4609d66E633959e0f9999999, 0x1ab07e29D2ec76117704944DfBDB9dE5b785E8f4, 0xD3358467C97906bA732d0ca4af20f14F0424CD4F]
+Y = [0.3, 11, 0.57, 120, 17]
 
 max_addr = 0xffffffffffffffffffffffffffffffffffffffff
+
+def test(x):
+    x = abs(x)
+    x = x + 1
+    x = x//1 
 '''
