12087d1d06858b6ab27a3258cd1d2793a2e7d02c
Shankar <shankars0899@gmail.com>
2022-10-31 19:52:56 -0400

    added comments

--- lagrange-consensus/lagrange.py
+++ lagrange-consensus/lagrange.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 # Define a function :
-# LagrangePolynomial(x) = F(x).D(x) = challengeBit(x)
+# LagrangePolynomial(x) = F(x).D(x) = key(x)
 # where F(x) is the Lagrange polynomial
 # and D(x) is the test if the public address is a member of the polynomial
-# challengeBit(x) is the randomly generated bit used as key for the user to claim the tokens
+# key(x) is the randomly generated bit used as key for the user to claim the tokens
 
 class LagrangePolynomial:
 
@@ -22,27 +22,32 @@ class LagrangePolynomial:
 
         assert(len(X) == len(self.D_Y))
 
+    # lagrange basis polynomial
     def Dx(self, x, j) -> np.ndarray:
         b = [(x - self.X[m]) / (self.X[j] - self.X[m])
              for m in range(self.n) if m != j]
 
         return np.prod(b, axis=0) * self.D_Y[j]
 
+    # returns 1 or 0 if the public address is a member of the polynomial
     def Fx(self, Dy) -> int:
         return 1 if Dy/int(Dy) == 1 else 0
 
-    def returnChallenge(self, x) -> int:
+    # returns the key for a public address
+    def returnKey(self, x) -> int:
         Dy = np.sum([self.Dx(x, j) for j in range(self.n)], axis=0)
         Fy = self.Fx(Dy)
 
         return int(abs(Dy * Fy)[0])
 
+    # returns if the public address matched with the right key
     def finalize(self, x, y) -> int:
         if y != 0 :
-            return 1 if self.returnChallenge(x) == y else 0
+            return 1 if self.returnKey(x) == y else 0
         else:
             return 0
 
+    # returns a random number
     def randNum(self) -> np.ndarray:
         return np.random.randint(low=0xFF, high=0xFFFF, size=1, dtype=int)
 
