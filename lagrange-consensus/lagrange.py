83d00c13ed8d2b3741f762c38b1ac2b693084ac3
Shankar <shankars0899@gmail.com>
2022-10-31 22:14:51 -0400

    changed location of returnGroupAddress()

--- lagrange-consensus/lagrange.py
+++ lagrange-consensus/lagrange.py
@@ -8,60 +8,60 @@ import numpy as np
 
 class LagrangePolynomial:
 
-    def __init__(self, X):
+    def __init__(self, X, alpha):
         self.n = len(X)
 
         self.X = np.zeros(self.n)
-        self.D_Y = np.zeros(self.n)
+        self.__D_Y = np.zeros(self.n)
 
-        self.modAddress = {}
+        self.__groupAddresses = {}
 
         key = 1
         ctr = 0
         for address in X:
-            key = self.randNum()
+            key = self.__randNum()
             while address % key == 0:
-                key = self.randNum()
+                key = self.__randNum()
 
-            # add exponentiation of address for the group
             modAddr = address % key
-            self.X[ctr] = modAddr
-            self.D_Y[ctr] = key
-            self.modAddress[address] = modAddr
+            groupAddr = alpha ** (modAddr) % key
+
+            self.X[ctr] = groupAddr
+            self.__D_Y[ctr] = key
+            self.__groupAddresses[address] = groupAddr
             ctr += 1
 
-        assert(len(X) == len(self.D_Y))
+        assert(len(X) == len(self.__D_Y))
 
     # lagrange basis polynomial
-    def Dx(self, x, j) -> np.ndarray:
+    def __Dx(self, x, j) -> np.ndarray:
         b = [(x - self.X[m]) / (self.X[j] - self.X[m])
              for m in range(self.n) if m != j]
 
-        return np.prod(b, axis=0) * self.D_Y[j]
+        return np.prod(b, axis=0) * self.__D_Y[j]
 
     # returns 1 or 0 if the public address is a member of the polynomial
-    def Fx(self, Dy) -> int:
+    def __Fx(self, Dy) -> int:
         return 1 if Dy/int(Dy) == 1 else 0
 
     # returns the key for a public address
     def returnKey(self, x) -> int:
-        # x = self.returnModAddress(x)
-        Dy = np.sum([self.Dx(x, j) for j in range(self.n)], axis=0)
-        Fy = self.Fx(Dy)
+        x = self.returnGroupAddress(x)
+        Dy = np.sum([self.__Dx(x, j) for j in range(self.n)], axis=0)
+        Fy = self.__Fx(Dy)
 
         return int(abs(Dy * Fy))
 
     # returns if the public address matched with the right key
     def finalize(self, x, y) -> int:
-        x = self.returnModAddress(x)
         if y != 0 :
             return 1 if self.returnKey(x) == y else 0
         else:
             return 0
 
     # returns a random number
-    def randNum(self) -> np.ndarray:
+    def __randNum(self) -> np.ndarray:
         return np.random.randint(low=0xFF, high=0xFFFF, size=1, dtype=int)
 
-    def returnModAddress(self, x) -> float:
-        return self.modAddress[x][0]
+    def returnGroupAddress(self, x) -> float:
+        return self.__groupAddresses[x][0]
