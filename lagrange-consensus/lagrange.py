ec00a356549ff35d65ffba463a2d7ca0f4f124cd
Shankar <shankars0899@gmail.com>
2022-11-01 10:18:25 -0400

    key is now a hash of the key with the groupAddress

--- lagrange-consensus/lagrange.py
+++ lagrange-consensus/lagrange.py
@@ -1,4 +1,7 @@
+from hashlib import sha256
 import numpy as np
+from math import ceil
+from hashlib import sha256
 
 # Define a function :
 # LagrangePolynomial(x) = F(x).D(x) = key(x)
@@ -13,6 +16,7 @@ class LagrangePolynomial:
         self.alpha = 131
         self.X = np.zeros(self.n)
         self.__D_Y = np.zeros(self.n)
+        self.__F_Y = np.ones(self.n)
 
         self.__groupAddressMapping = {}
 
@@ -47,14 +51,16 @@ class LagrangePolynomial:
         b = [(x - self.X[m]) / (self.X[j] - self.X[m])
              for m in range(self.n) if m != j]
 
-        return np.prod(b, axis=0) * self.__D_Y[j]
+        return np.prod(b, axis=0) * self.__F_Y[j]
 
     # returns the key for a public address
-    def returnKey(self, x) -> int:
+    def returnKey(self, x):
         Dy = np.sum([self.__Dx(x, j) for j in range(self.n)], axis=0)
         Fy = np.sum([self.__Fx(x, j) for j in range(self.n)], axis=0)
-
-        return int(abs(Dy * Fy))
+        # Fy = 1 - ceil((1/(1 + 2**(-Fy)) - 0.5))
+        # return Fy
+        n = int(abs(Dy * Fy) * 2 ** 40)
+        return sha256(str(n ^ x).encode()).hexdigest()
 
     # returns if the public address matched with the right key
     def finalize(self, x, y) -> int:
