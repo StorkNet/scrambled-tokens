321bdb7cf0f71ddda448d6db36ff37c47643f767
Shankar <shankars0899@gmail.com>
2022-10-31 21:17:41 -0400

    using modAddr works - add group operation

--- lagrange-consensus/lagrange.py
+++ lagrange-consensus/lagrange.py
@@ -10,15 +10,25 @@ class LagrangePolynomial:
 
     def __init__(self, X):
         self.n = len(X)
-        self.X = np.array(X)
 
-        self.D_Y = []
+        self.X = np.zeros(self.n)
+        self.D_Y = np.zeros(self.n)
 
-        for i in range(self.n):
-            key = 1
-            while self.X[i] % key == 0:
+        self.modAddress = {}
+
+        key = 1
+        ctr = 0
+        for address in X:
+            key = self.randNum()
+            while address % key == 0:
                 key = self.randNum()
-            self.D_Y.append(key)
+
+            # add exponentiation of address for the group
+            modAddr = address % key
+            self.X[ctr] = modAddr
+            self.D_Y[ctr] = key
+            self.modAddress[address] = modAddr
+            ctr += 1
 
         assert(len(X) == len(self.D_Y))
 
@@ -35,13 +45,15 @@ class LagrangePolynomial:
 
     # returns the key for a public address
     def returnKey(self, x) -> int:
+        # x = self.returnModAddress(x)
         Dy = np.sum([self.Dx(x, j) for j in range(self.n)], axis=0)
         Fy = self.Fx(Dy)
 
-        return int(abs(Dy * Fy)[0])
+        return int(abs(Dy * Fy))
 
     # returns if the public address matched with the right key
     def finalize(self, x, y) -> int:
+        x = self.returnModAddress(x)
         if y != 0 :
             return 1 if self.returnKey(x) == y else 0
         else:
@@ -51,8 +63,5 @@ class LagrangePolynomial:
     def randNum(self) -> np.ndarray:
         return np.random.randint(low=0xFF, high=0xFFFF, size=1, dtype=int)
 
-
-
-'''
-X = [0x6be986730E72fCd23910D66A1722b4b3611e50D2, 0x07F20B1265059fEb39D605e8476aa51595483Ec6, 0x5F45498761247C2A4609d66E633959e0f9999999, 0x1ab07e29D2ec76117704944DfBDB9dE5b785E8f4, 0xD3358467C97906bA732d0ca4af20f14F0424CD4F]
-'''
+    def returnModAddress(self, x) -> float:
+        return self.modAddress[x][0]
