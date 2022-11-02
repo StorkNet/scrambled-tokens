550925f29c2ef068e1ec208fdd23e8276b9dd8b4
Shankar <shankars0899@gmail.com>
2022-11-01 23:05:49 -0400

    added fail case

--- lagrange-consensus/lagrange_test.py
+++ lagrange-consensus/lagrange_test.py
@@ -1,13 +1,23 @@
 import lagrange
 import numpy as np
+from hashlib import sha256
+
+def randNum():
+    return np.random.randint(low=0xF001, high=0xFFFF, size=1, dtype=int)[0]
+
+def printSummary(summary):
+    print("\n--------------------------------------\nSummary:\n" + summary)
 
 print("Testing lagrange.py")
-X = [0x6be986730E72fCd23910D66A1722b4b3611e50D2, 0x07F20B1265059fEb39D605e8476aa51595483Ec6,
-     0x5F45498761247C2A4609d66E633959e0f9999999, 0x1ab07e29D2ec76117704944DfBDB9dE5b785E8f4, 0xD3358467C97906bA732d0ca4af20f14F0424CD4F]
+X_honest = [0x6be986730E72fCd23910D66A1722b4b3611e50D2,
+            0x07F20B1265059fEb39D605e8476aa51595483Ec6]
+
+X_attack = [0x1ab07e29D2ec76117704944DfBDB9dE5b785E8f4,
+            0xD3358467C97906bA732d0ca4af20f14F0424CD4F]
 
 print("Initializing lagrange object")
-alpha = np.random.randint(low=0x03, high=0xF, size=1, dtype=int)[0]
-lp = lagrange.LagrangePolynomial(X)
+alpha = 131
+lp = lagrange.LagrangePolynomial(X_honest)
 
 # def ModAddressTest():
 # lagrange_test.alpha ** (lagrange_test.X[0] % lagrange_test.lp.returnKey(lagrange_test.X[0])) % lagrange_test.lp.returnKey(lagrange_test.X[0])
@@ -17,11 +27,20 @@ lp = lagrange.LagrangePolynomial(X)
 
 
 def FinalizeTest():
-    for x in X:
-        # print("Testing address: 0x%x" % x)
-        # print("Group address: 0x%x" % lp.returnGroupAddress(x))
-        # print("Key: 0x%s" % lp.returnKey(lp.returnGroupAddress(x)))
-        # print("Finalize: %d" % lp.finalize(lp.returnGroupAddress(x), lp.returnKey(lp.returnGroupAddress(x))))
-        # print("")
-        assert(lp.finalize(lp.returnGroupAddress(x), lp.returnKey(lp.returnGroupAddress(x))) == 1)
-    print("All group addresses finalized with 1")
+    for x_honest in X_honest:
+        print("Testing address: 0x%x" % x_honest)
+        print("Group address: 0x%x" % lp.returnGroupAddress(x_honest))
+        print("Key: 0x%s" % lp.returnKey(lp.returnGroupAddress(x_honest)))
+        print("")
+        assert(lp.finalize(lp.returnGroupAddress(x_honest),
+               lp.returnKey(lp.returnGroupAddress(x_honest))) == 1)
+
+    for x_attack in X_attack:
+        print("Testing address: 0x%x" % x_attack)
+        print("Key: 0x%s" % lp.returnKey(x_attack))
+        print("Finalize: %d" % lp.finalize(
+            x_attack, lp.returnKey(x_attack)))
+        print("")
+        assert(lp.returnKey(x_attack) == ''.zfill(64))
+
+    printSummary("All honest group addresses finalized with 1 \nAll attack group addresses finalized with 0")
