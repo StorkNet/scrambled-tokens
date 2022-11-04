03066458db41fb4cf122ddaf540d6e777fdc6ac9
Shankar <shankars0899@gmail.com>
2022-11-03 20:35:09 -0400

    using secp256k1 for converting ethPublicKey address to storkAddress:

---
+++ lagrange-consensus/secp256k1.py
@@ -0,0 +1,8 @@
+from hashlib import sha256
+
+def generateKey(publicKeyEth) :
+    p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+    a = 0x0000000000000000000000000000000000000000000000000000000000000000
+    b = 0x0000000000000000000000000000000000000000000000000000000000000007
+    storkKey = ( (publicKeyEth**3 + publicKeyEth*a + b) % p ) ** 0.5
+    return sha256((str(publicKeyEth) + "scrambled-egg").encode()).hexdigest()
