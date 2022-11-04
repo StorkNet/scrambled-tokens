2fea566759aea4a4cdb68e58b5bd81e581c9c8ed
Shankar <shankars0899@gmail.com>
2022-11-03 20:44:10 -0400

    returns a 40 bit address

--- lagrange-consensus/secp256k1.py
+++ lagrange-consensus/secp256k1.py
@@ -5,4 +5,4 @@ def generateKey(publicKeyEth) :
     a = 0x0000000000000000000000000000000000000000000000000000000000000000
     b = 0x0000000000000000000000000000000000000000000000000000000000000007
     storkKey = ( (publicKeyEth**3 + publicKeyEth*a + b) % p ) ** 0.5
-    return sha256((str(publicKeyEth) + "scrambled-egg").encode()).hexdigest()
+    return sha256((str(publicKeyEth) + "scrambled-egg").encode()).hexdigest()[:40]
